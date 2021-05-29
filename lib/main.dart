6556a2b0775d097c565b721d360d2ef6ead8bbf8
Nayab <nayabnadeem621@gmail.com>
2021-05-30 04:52:59 +0500

    uodate

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,39 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { 
-    return MaterialApp (
-     home: (
-       Scaffold(body: Text("Nayab Nadeem Sheikh"
-       ))
-     ), 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+         home: FirstScreen(),
+         debugShowCheckedModeBanner: false,  
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+            title: Text('new app'),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Center(
+              child: Container(
+                width: 300,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+
+              ],
+            ) ,
+            ),
     );
   }
 }
